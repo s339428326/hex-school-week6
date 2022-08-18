c961862c9f09c0195d02407e17b5d7a151fda6f3
翁柏豐 <s339428326@gmail.com>
2022-08-19 00:09:17 +0800

    index page first version

---
+++ app/assets/js/all.js
@@ -0,0 +1,39 @@
+var swiper = new Swiper(".swiper-container", {
+  pagination: ".swiper-pagination",
+  breakpoints: {
+    991: {
+      slidesPerView: 10,
+      spaceBetween: 16,
+    },
+    768: {
+      slidesPerView: 5,
+      spaceBetween: 16,
+    },
+    0: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+  },
+  paginationClickable: true,
+  freeMode: true,
+});
+
+const swiper_1 = new Swiper(".swiper_slider", {
+  spaceBetween: 30,
+  effect: "fade",
+  // 分頁
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  // 左右箭頭
+  navigation: {
+    nextEl: ".swiper-next",
+    prevEl: ".swiper-prev",
+  },
+  // 滾動條
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    draggable: true,
+  },
+});
